0e73f1ef0d0376b714bbe2b0f406ce1d6872bbce
unknown <kuntalavanya122@gmail.com>
2023-06-17 16:21:51 +0530

    level-4 encryption

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const ejs=require('ejs');
 const mongoose=require('mongoose');
-const encrypt=require("mongoose-encryption");
+//const encrypt=require("mongoose-encryption");
+//const md5=require("md5"); to increse mo security use bcrypt instead of md5       |i forgot to commit in git   level3 security
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 
 const app=express();
 
-console.log(process.env.API_KEY);
+//console.log(md5("123456"));  //hash pasword      level3 security
+
+//console.log(process.env.API_KEY);                 ""
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -31,7 +36,7 @@ const userSchema= new mongoose.Schema({
 });
 //const secret="Thisisourlittlesecrete.";
 //userSchema.plugin(encrypt,{secret:secret, encryptFields:["password"]});
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=new mongoose.model('User',userSchema);
 
@@ -48,33 +53,42 @@ app.get("/register",function(req,res){
 
 
 app.post("/register",function(req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:req.body.password
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser=new User({
+      email:req.body.username,
+      //password:md5(req.body.password)   //level3 security
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("secrets")
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.render("secrets")
-    }
-  });
+
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
+  //const password=md5(req.body.password);   level 3 security
   const password=req.body.password;
-
   User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log(err);
     }
     else{
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
-        }
+        //if(foundUser.password===password){
+          //res.render("secrets");
+        //}
+        bcrypt.compare(password,foundUser.password,function(err,result){
+          if(result===true){
+            res.render("secrets");
+          }
+        })
       }
     }
   });
